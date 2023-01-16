84bc54ca260489dc522bd278919cf20395b2caee
Sergey Denisyuk <sergey.denisyuk@mimy.online>
2023-01-16 15:34:31 +0200

    Implement registration page

--- src/routes/registration/registration.page.tsx
+++ src/routes/registration/registration.page.tsx
@@ -1,3 +1,61 @@
+import {
+  Button,
+  Container,
+  Divider,
+  Group,
+  PasswordInput,
+  TextInput,
+  Title,
+} from '@mantine/core'
+import { useRouter } from 'src/router'
+import useRegistration from './use-registration'
+
 export function RegistrationPage() {
-  return <div>Registration Page</div>
+  const router = useRouter()
+  const { form, submit } = useRegistration()
+
+  function onSubmit(event: React.FormEvent) {
+    event.preventDefault()
+    submit()()
+  }
+
+  return (
+    <Container
+      style={{
+        maxWidth: 450,
+        width: '100%',
+        height: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        gap: 30,
+      }}
+    >
+      <Title order={1}>Registration</Title>
+      <form onSubmit={onSubmit} style={{ width: '100%' }}>
+        <TextInput
+          placeholder="Username"
+          {...form.getInputProps('username')}
+          h={65}
+        />
+
+        <PasswordInput
+          placeholder="Password"
+          {...form.getInputProps('password')}
+          h={65}
+        />
+
+        <Group position="center" mt="xs" spacing={2}>
+          <Button type="submit" fullWidth>
+            Register
+          </Button>
+          <Divider w={'100%'} my="xs" label="or" labelPosition="center" />
+          <Button component="a" {...router.routes.login().link} fullWidth>
+            Login
+          </Button>
+        </Group>
+      </form>
+    </Container>
+  )
 }
---
+++ src/routes/registration/use-registration.ts
@@ -0,0 +1,41 @@
+import { useForm } from '@mantine/form'
+import { useRef } from 'react'
+import { passwordValidation, userNameValidation } from './validation'
+
+type FormValues = {
+  username: string
+  password: string
+}
+
+function useRegistration() {
+  const hasSubmitted = useRef(false)
+
+  const form = useForm<FormValues>({
+    initialValues: {
+      username: '',
+      password: '',
+    },
+
+    validate: {
+      username: (value) => userNameValidation(value, hasSubmitted.current),
+      password: (value) => passwordValidation(value, hasSubmitted.current),
+    },
+    validateInputOnChange: true,
+  })
+
+  function submit() {
+    return () => {
+      hasSubmitted.current = true
+      form.onSubmit((values) => {
+        console.log(values)
+      })()
+    }
+  }
+
+  return {
+    submit,
+    form,
+  }
+}
+
+export default useRegistration
