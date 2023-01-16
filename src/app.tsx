5e12e1586ba8218ccedde3865a81797235704d07
denisyuk26 <sergey.denisyuk@gmail.com>
2023-01-16 12:45:12 +0200

    Remove tailwind from app (#4)
    fix: Remove tailwind from app

--- src/app.tsx
+++ src/app.tsx
@@ -1,32 +1,10 @@
-import { useMemo, useState } from 'react'
-import BaseLayout from './components/base-layout/base-layout'
-import LoginCardContext from './components/log-in-card/log-in-card-context'
 import MuscleGroupPicker from './components/muscle-group-picker/muscle-group-picker'
 
 function App() {
-  const [isLoginCardOpened, setIsLoginCardOpened] = useState(true)
-  const loginContextValue = useMemo(
-    () => ({
-      closeLoginCard: () => {
-        setIsLoginCardOpened(false)
-      },
-      openLoginCard: () => {
-        setIsLoginCardOpened(true)
-      },
-      isLoginCardOpened,
-    }),
-    [isLoginCardOpened],
-  )
   return (
-    <LoginCardContext.Provider value={loginContextValue}>
-      <BaseLayout>
-        <div className="text-center bg-white flex h-full justify-center items-center flex-col gap-4">
-          {/* <LoginCard contextValue={loginContextValue} /> */}
-
-          <MuscleGroupPicker />
-        </div>
-      </BaseLayout>
-    </LoginCardContext.Provider>
+    <div>
+      <MuscleGroupPicker />
+    </div>
   )
 }
 
--- src/components/card/card.tsx
+++
@@ -1,9 +0,0 @@
-function Card(props: { children: React.ReactNode }) {
-  return (
-    <div className="p-6 max-w-md w-full bg-white rounded-lg sm:max-w-full">
-      {props.children}
-    </div>
-  )
-}
-
-export default Card
