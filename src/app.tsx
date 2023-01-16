1ee0a325573c7b2a06fda84705f829d91f134154
Anatolii Domaratskyi <domaratskiyas@gmail.com>
2023-01-16 13:02:28 +0200

    Add type-route

--- src/app.tsx
+++ src/app.tsx
@@ -1,33 +1,7 @@
-import { useMemo, useState } from 'react'
-import BaseLayout from './components/base-layout/base-layout'
-import LoginCardContext from './components/log-in-card/log-in-card-context'
-import MuscleGroupPicker from './components/muscle-group-picker/muscle-group-picker'
+import { Routes } from 'src/routes/routes'
 
 function App() {
-  const [isLoginCardOpened, setIsLoginCardOpened] = useState(true)
-  const loginContextValue = useMemo(
-    () => ({
-      closeLoginCard: () => {
-        setIsLoginCardOpened(false)
-      },
-      openLoginCard: () => {
-        setIsLoginCardOpened(true)
-      },
-      isLoginCardOpened,
-    }),
-    [isLoginCardOpened],
-  )
-  return (
-    <LoginCardContext.Provider value={loginContextValue}>
-      <BaseLayout>
-        <div className="text-center bg-white flex h-full justify-center items-center flex-col gap-4">
-          {/* <LoginCard contextValue={loginContextValue} /> */}
-
-          <MuscleGroupPicker />
-        </div>
-      </BaseLayout>
-    </LoginCardContext.Provider>
-  )
+  return <Routes />
 }
 
 export default App
