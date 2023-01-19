74f2c206ca3079222307107f604e6590529557a7
Sergey Denisyuk <sergey.denisyuk@mimy.online>
2023-01-19 11:40:34 +0200

    PR fix

--- src/routes/stats/stats.page.tsx
+++ src/routes/stats/stats.page.tsx
@@ -1,54 +1,15 @@
 import { AppShell } from 'src/components/app-shell/app-shell'
-import Breadcrumbs from 'src/components/breadcrumbs/breadcrumbs'
 import DataCard from 'src/components/data-card/data-card'
 import { useAuthRoute } from 'src/hooks/use-auth-route'
-import { useBreadCrumbs } from 'src/hooks/use-breadcrumbs'
-import { useRouter } from 'src/router'
 
 export function StatsPage() {
   useAuthRoute()
-  const router = useRouter()
-
-  const { showBreadcrumbs, handleHideBreadcrumbs, handleShowBreadcrumbs } =
-    useBreadCrumbs()
 
   return (
-    <AppShell
-      title={'Stats'}
-      leftSlot={
-        showBreadcrumbs ? (
-          <Breadcrumbs
-            {...router.routes.stats().link}
-            onClick={() => {
-              handleHideBreadcrumbs()
-              router.routes.stats().link.onClick()
-            }}
-            label="Stats"
-          />
-        ) : null
-      }
-    >
-      <DataCard
-        title={'Stat 1'}
-        content={<div>Stat: 1</div>}
-        fullContent={<div>stats 1</div>}
-        onClick={handleShowBreadcrumbs}
-        isBreadcrumbShowed={showBreadcrumbs}
-      ></DataCard>
-      <DataCard
-        title={'Stat 2'}
-        content={<div>Stat: 2</div>}
-        fullContent={<div>stats 2</div>}
-        onClick={handleShowBreadcrumbs}
-        isBreadcrumbShowed={showBreadcrumbs}
-      ></DataCard>
-      <DataCard
-        title={'Stat 3'}
-        content={<div>Stat: 3</div>}
-        fullContent={<div>stats 3</div>}
-        onClick={handleShowBreadcrumbs}
-        isBreadcrumbShowed={showBreadcrumbs}
-      ></DataCard>
+    <AppShell title={'Stats'}>
+      <DataCard title={'Stat 1'} content={<div>Stat: 1</div>}></DataCard>
+      <DataCard title={'Stat 2'} content={<div>Stat: 2</div>}></DataCard>
+      <DataCard title={'Stat 3'} content={<div>Stat: 3</div>}></DataCard>
     </AppShell>
   )
 }
