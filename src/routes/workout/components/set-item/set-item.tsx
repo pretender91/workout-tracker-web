f8463f5923e1772dab7ef9b73ef00d330abe593e
Sergey Denisyuk <sergey.denisyuk@mimy.online>
2023-02-09 12:47:15 +0200

    Add dnd

--- src/routes/workout/components/set-item/set-item.tsx
+++ src/routes/workout/components/set-item/set-item.tsx
@@ -7,6 +7,7 @@ import {
   Title,
 } from '@mantine/core'
 import { IconCirclePlus, IconX } from '@tabler/icons'
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd'
 import { Exercise } from '../workout-card/use-workout-card'
 import { useSetItem } from './use-set-item'
 
@@ -33,65 +34,107 @@ export function SetItem(props: {
           <Title order={5}>{props.item.name}</Title>
         </Accordion.Control>
         <Accordion.Panel>
-          <form onSubmit={onSubmit}>
-            {form.values.data.map((_, index) => {
-              return (
-                <Flex
-                  direction="column"
-                  gap={10}
-                  justify="space-between"
-                  key={index}
-                  style={{ margin: '10px 0' }}
-                >
-                  <Title order={6}>Set {index + 1}</Title>
-                  <Flex gap={10} justify="space-between" align="center">
-                    <NumberInput
-                      style={{
-                        maxWidth: '135px',
-                      }}
-                      size="xs"
-                      {...form.getInputProps(`data.${index}.reps`)}
-                      placeholder="Repetitions"
-                    />
-                    <NumberInput
-                      size="xs"
-                      {...form.getInputProps(`data.${index}.weight`)}
-                      placeholder="Weight"
-                      icon="Kg"
-                      style={{
-                        maxWidth: '135px',
-                      }}
-                    />
+          <DragDropContext
+            onDragEnd={({ destination, source }) => {
+              if (!destination) {
+                return
+              }
+              form.reorderListItem('data', {
+                from: source.index,
+                to: destination.index,
+              })
+            }}
+          >
+            <form onSubmit={onSubmit}>
+              <Droppable droppableId="dnd-list" direction="vertical">
+                {(provided) => (
+                  <div {...provided.droppableProps} ref={provided.innerRef}>
+                    {form.values.data.map((_, index) => {
+                      return (
+                        <Draggable
+                          key={index}
+                          index={index}
+                          draggableId={index.toString()}
+                        >
+                          {(provided) => (
+                            <Flex
+                              direction="column"
+                              gap={10}
+                              justify="space-between"
+                              key={index}
+                              style={{ margin: '10px 0' }}
+                              ref={provided.innerRef}
+                              {...provided.draggableProps}
+                              {...provided.dragHandleProps}
+                            >
+                              <Title order={6}>Set {index + 1}</Title>
+                              <Flex
+                                gap={10}
+                                justify="space-between"
+                                align="center"
+                              >
+                                <NumberInput
+                                  style={{
+                                    maxWidth: '135px',
+                                  }}
+                                  size="xs"
+                                  {...form.getInputProps(`data.${index}.reps`)}
+                                  placeholder="Repetitions"
+                                />
+                                <NumberInput
+                                  size="xs"
+                                  {...form.getInputProps(
+                                    `data.${index}.weight`,
+                                  )}
+                                  placeholder="Weight"
+                                  icon="Kg"
+                                  style={{
+                                    maxWidth: '135px',
+                                  }}
+                                />
 
-                    <ActionIcon
-                      disabled={index === 0 && form.values.data.length <= 1}
-                      onClick={() => removeSetItem(index)}
-                      color="red"
-                      size={36}
-                      style={{
-                        alignSelf: 'flex-end',
-                        backgroundColor: 'transparent',
-                        borderColor: 'transparent',
-                        marginRight: '-11px',
-                      }}
-                    >
-                      <IconX size={20} />
-                    </ActionIcon>
-                  </Flex>
-                </Flex>
-              )
-            })}
+                                <ActionIcon
+                                  disabled={
+                                    index === 0 && form.values.data.length <= 1
+                                  }
+                                  onClick={() => removeSetItem(index)}
+                                  color="red"
+                                  size={36}
+                                  style={{
+                                    alignSelf: 'flex-end',
+                                    backgroundColor: 'transparent',
+                                    borderColor: 'transparent',
+                                    marginRight: '-11px',
+                                  }}
+                                >
+                                  <IconX size={20} />
+                                </ActionIcon>
+                              </Flex>
+                            </Flex>
+                          )}
+                        </Draggable>
+                      )
+                    })}
+                    {provided.placeholder}
+                    <Flex direction="column" gap={20}>
+                      <ActionIcon onClick={addReps}>
+                        <IconCirclePlus size={36} color="#228be6" />
+                      </ActionIcon>
 
-            <Flex direction="column" gap={20}>
-              <ActionIcon onClick={addReps}>
-                <IconCirclePlus size={36} color="#228be6" />
-              </ActionIcon>
-
-              <Button fullWidth color="green" variant="outline" type="submit">
-                Commit
-              </Button>
-            </Flex>
-          </form>
+                      <Button
+                        fullWidth
+                        color="green"
+                        variant="outline"
+                        type="submit"
+                      >
+                        Commit
+                      </Button>
+                    </Flex>
+                  </div>
+                )}
+              </Droppable>
+            </form>
+          </DragDropContext>
         </Accordion.Panel>
       </Accordion.Item>
     </Accordion>
--- src/routes/workout/components/superset-item/superset-item.tsx
+++ src/routes/workout/components/superset-item/superset-item.tsx
@@ -7,6 +7,7 @@ import {
   Title,
 } from '@mantine/core'
 import { IconCirclePlus, IconX } from '@tabler/icons'
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd'
 import { Exercise } from '../workout-card/use-workout-card'
 import { useSupersetItem } from './use-superset-item'
 
@@ -34,85 +35,124 @@ export function SupersetItem(props: {
           <Title order={5}>{formattedName} (Superset)</Title>
         </Accordion.Control>
         <Accordion.Panel>
-          <form onSubmit={onSubmit}>
-            <div>
-              {form.values.items.map((group, groupIndex) => {
-                return (
-                  <Flex
-                    key={groupIndex}
-                    direction="column"
-                    gap={10}
-                    justify="space-between"
-                    style={{
-                      position: 'relative',
-                      margin: '10px 0',
-                    }}
-                  >
-                    <Title order={6}>Set {groupIndex + 1}</Title>
-                    {group.map((item, index) => {
+          <DragDropContext
+            onDragEnd={({ destination, source }) => {
+              if (!destination) {
+                return
+              }
+              form.reorderListItem('data', {
+                from: source.index,
+                to: destination.index,
+              })
+            }}
+          >
+            <form onSubmit={onSubmit}>
+              <Droppable droppableId="dnd-list" direction="vertical">
+                {(provided) => (
+                  <div {...provided.droppableProps} ref={provided.innerRef}>
+                    {form.values.items.map((group, groupIndex) => {
                       return (
-                        <div key={index}>
-                          <Title order={6}>{item.name}</Title>
-                          <Flex gap={10} justify="space-between" align="center">
-                            <NumberInput
-                              style={{
-                                maxWidth: '135px',
-                              }}
-                              size="xs"
-                              {...form.getInputProps(
-                                `items.${groupIndex}.${index}.reps`,
-                              )}
-                              placeholder="Repetitions"
-                            />
-                            <NumberInput
-                              size="xs"
-                              {...form.getInputProps(
-                                `items.${groupIndex}.${index}.wieght`,
-                              )}
-                              placeholder="Weight"
-                              icon="Kg"
-                              style={{
-                                maxWidth: '135px',
-                              }}
-                            />
-                          </Flex>
-                        </div>
+                        <Draggable
+                          key={groupIndex}
+                          index={groupIndex}
+                          draggableId={groupIndex.toString()}
+                        >
+                          {(provided) => (
+                            <Flex
+                              key={groupIndex}
+                              direction="column"
+                              gap={5}
+                              justify="space-between"
+                              ref={provided.innerRef}
+                              {...provided.draggableProps}
+                            >
+                              <Flex
+                                {...provided.dragHandleProps}
+                                justify="space-between"
+                                align="center"
+                                style={{
+                                  marginTop: '10px',
+                                }}
+                              >
+                                <Title order={6}>Set {groupIndex + 1}</Title>
+                                <ActionIcon
+                                  disabled={
+                                    groupIndex === 0 &&
+                                    form.values.items.length <= 1
+                                  }
+                                  onClick={() => removeFormItem(groupIndex)}
+                                  color="red"
+                                  style={{
+                                    alignSelf: 'flex-end',
+                                    backgroundColor: 'transparent',
+                                    borderColor: 'transparent',
+                                  }}
+                                >
+                                  <IconX size={20} />
+                                </ActionIcon>
+                              </Flex>
+
+                              {group.map((item, index) => {
+                                return (
+                                  <div key={index}>
+                                    <Title order={6}>{item.name}</Title>
+                                    <Flex
+                                      gap={10}
+                                      justify="space-between"
+                                      align="center"
+                                    >
+                                      <NumberInput
+                                        style={{
+                                          maxWidth: '135px',
+                                        }}
+                                        size="xs"
+                                        {...form.getInputProps(
+                                          `items.${groupIndex}.${index}.reps`,
+                                        )}
+                                        placeholder="Repetitions"
+                                      />
+                                      <NumberInput
+                                        size="xs"
+                                        {...form.getInputProps(
+                                          `items.${groupIndex}.${index}.wieght`,
+                                        )}
+                                        placeholder="Weight"
+                                        icon="Kg"
+                                        style={{
+                                          maxWidth: '135px',
+                                        }}
+                                      />
+                                    </Flex>
+                                  </div>
+                                )
+                              })}
+                            </Flex>
+                          )}
+                        </Draggable>
                       )
                     })}
+                    {provided.placeholder}
+                  </div>
+                )}
+              </Droppable>
 
-                    <ActionIcon
-                      disabled={
-                        groupIndex === 0 && form.values.items.length <= 1
-                      }
-                      onClick={() => removeFormItem(groupIndex)}
-                      color="red"
-                      size={36}
-                      style={{
-                        alignSelf: 'flex-end',
-                        backgroundColor: 'transparent',
-                        borderColor: 'transparent',
-                        marginRight: '-11px',
-                        position: 'absolute',
-                        right: 0,
-                        top: 5,
-                      }}
-                    >
-                      <IconX size={20} />
-                    </ActionIcon>
-                  </Flex>
-                )
-              })}
-            </div>
-            <Flex direction="column" gap={20}>
-              <ActionIcon onClick={addReps}>
-                <IconCirclePlus size={36} color="#228be6" />
-              </ActionIcon>
+              <Flex
+                direction="column"
+                gap={20}
+                style={{
+                  marginTop: '20px',
+                }}
+              >
+                <ActionIcon onClick={addReps}>
+                  <IconCirclePlus size={36} color="#228be6" />
+                </ActionIcon>
 
-              <Button fullWidth color="green" variant="outline" type="submit">
-                Commit
-              </Button>
-            </Flex>
-          </form>
+                <Button fullWidth color="green" variant="outline" type="submit">
+                  Commit
+                </Button>
+              </Flex>
+            </form>
+          </DragDropContext>
         </Accordion.Panel>
       </Accordion.Item>
     </Accordion>
